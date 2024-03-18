3c3970cc8b1ac30e7321cd980d57919c2b9f715a
ParkSeongGeun <phd0801@naver.com>
2024-03-18 22:02:05 +0900

    [Week1-ParkSeongGeun] Project 생성

---
+++ 03_seonggeun/Week1/FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 박성근 on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
