70830f65b9bfc7dc967392ae9a1e1474ea32658a
ParkSeongGeun <phd0801@naver.com>
2024-03-23 22:10:57 +0900

    [Week2-ParkSeongGeun] homework
    - 우선 생각나는 대로 view를 구현했는데..
    - refactoring후에 다시 commit 할 예정
    - UI 구현만 숙제인 거 같아서 UI까지만 함
    - Simulator 여러 개 비교(X)

--- 03_seonggeun/Week2/WeatherApp/WeatherApp/ContentView.swift
+++ 03_seonggeun/Week2/WeatherApp/WeatherApp/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
     let screenWidth = UIScreen.main.bounds.width
@@ -60,7 +61,7 @@ struct ContentView: View {
                             }
                         }
                     }.padding()
-                    .font(.system(size: 14))
+                        .font(.system(size: 14))
                         .foregroundColor(.white)
                         .background(.gray.opacity(0.5))
                         .cornerRadius(10)
@@ -77,7 +78,7 @@ struct ContentView: View {
                             Text("바탕으로 인해 체감 온도가 실제 온도보다 더 낮게 느껴집니다.")
                         }
                         .frame(width: screenWidth * 0.45, height: screenWidth * 0.45)
-                        .background(.gray.opacity(0.5))
+                        .background(Color(hex: 0x293355).opacity(0.5))
                         .cornerRadius(10)
                         
                         VStack(alignment: .leading, spacing: 25) {
@@ -90,13 +91,106 @@ struct ContentView: View {
                             Text("바탕으로 인해 체감 온도가 실제 온도보다 더 낮게 느껴집니다.")
                         }
                         .frame(width: screenWidth * 0.45, height: screenWidth * 0.45)
-                        .background(.gray.opacity(0.5))
+                        .background(Color(hex: 0x293355).opacity(0.5))
                         .cornerRadius(10)
-                    }.padding(.horizontal, 15)
-                        
+                    }
+                    .foregroundStyle(Color.white)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 15)
                 }
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.gray)
+                        Text("10일간의 일기예보")
+                            .foregroundStyle(.gray)
+                            .font(.system(size: 12))
+                        Spacer()
+                    }.padding(.top, 10)
+                        .padding(.leading, 15)
+                    Divider().background(.white).padding(.horizontal, 15)
+                    ForEach(1..<10) { _ in
+                        HStack(spacing: 50) {
+                            Text("오늘")
+                            Image(systemName: "moon.stars.fill")
+                            HStack {
+                                Text("0°")
+                                Image("stick")
+                                    .resizable()
+                                    .frame(width: screenWidth*0.3)
+                                Text("10°")
+                            }
+                        }
+                        .foregroundColor(.white)
+                        Divider().background(.white).padding(.horizontal, 15)
+                    }
+                    
+                }
+                .background(Color(hex: 0x292844))
+                .cornerRadius(10)
+                .padding(.horizontal, 15)
+                .padding(.top, 15)
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "moon.stars.fill")
+                        Text("상현망간의 달")
+                        Spacer()
+                    }
+                    .foregroundColor(.gray)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 15)
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
+                                Text("밝은 면")
+                                    .foregroundStyle(Color.white)
+                                Spacer()
+                                Text("74%")
+                            }
+                            Divider().background(Color.white)
+                            HStack {
+                                Text("월몰")
+                                    .foregroundStyle(Color.white)
+                                Spacer()
+                                Text("04:15")
+                            }
+                            Divider().background(Color.white)
+                            HStack {
+                                Text("다음 보름달")
+                                    .foregroundStyle(Color.white)
+                                Spacer()
+                                Text("5일")
+                            }
+                        }
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 15)
+                        
+                        Image("moon")
+                            .resizable()
+                            .frame(width: 130, height: 130)
+                            .padding(.trailing,  15)
+                    }
+                    .padding(.bottom, 15)
+                }
+                .font(.system(size: 14))
+                .background(Color(hex: 0x293355))
+                .cornerRadius(10)
+                .padding()
             }
         }
+    }
+}
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xff) / 255
+        let green = Double((hex >> 8) & 0xff) / 255
+        let blue = Double((hex >> 0) & 0xff) / 255
+
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
     }
 }
 
