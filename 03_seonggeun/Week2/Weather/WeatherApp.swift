315b4f80633e6e546988734cc60a38175b97694c
ParkSeongGeun <phd0801@naver.com>
2024-03-25 20:44:58 +0900

    [Week2-ParkSeongGeun] 2주차 실습과제
    -> branch 재생성 후
    -> main 틀에 추가

---
+++ 03_seonggeun/Week2/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 가은 on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
