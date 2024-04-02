8e4c55fd5c375f5e229acf643fbc6dac3ad83d99
Anjin Lee <anjin720jin@naver.com>
2024-04-02 17:16:33 +0900

    [Week03-anjin-study] Project 생성

---
+++ 01_anjin/Week03/SettingApp/SettingApp/SettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingAppApp.swift
+//  SettingApp
+//
+//  Created by Anjin on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
