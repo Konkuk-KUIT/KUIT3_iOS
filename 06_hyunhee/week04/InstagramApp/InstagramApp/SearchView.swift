e675fa56323588077685dc5b8c5f04dca08a4fde
nonaninona <starcraft0529@gmail.com>
2024-04-11 23:58:51 +0900

    grid 만들기

--- 06_hyunhee/week04/InstagramApp/InstagramApp/SearchView.swift
+++ 06_hyunhee/week04/InstagramApp/InstagramApp/SearchView.swift
@@ -9,21 +9,62 @@ import SwiftUI
 
 struct SearchView: View {
     @State private var searchText: String = ""
+    private let columns : [GridItem] = [
+        GridItem(.flexible())
+    ]
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(height: 50)
-                .padding(.horizontal)
-                .foregroundColor(Color(.systemGray6))
-                .overlay {
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                        TextField("검색", text: $searchText)
-                        Text("\(searchText.count)")
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(height: 50)
+                        .padding(.horizontal)
+                        .foregroundColor(Color(.systemGray6))
+                        .overlay {
+                            HStack {
+                                Image(systemName: "magnifyingglass")
+                                TextField("검색", text: $searchText)
+                                Text("\(searchText.count)")
+                            }
+                            .padding(30)
+                        }
+                    LazyVGrid(columns: columns, spacing: 1) {
+                        ForEach(0..<5){ _ in
+                            Grid(horizontalSpacing: 1) {
+                                GridRow() {
+                                    Image("sampleImage")
+                                        .resizable()
+                                        .frame(width: geometry.size.width/3, height: geometry.size.width/3*2 + 1)
+                                        .scaledToFit()
+                                    Grid(horizontalSpacing: 1, verticalSpacing: 1) {
+                                        GridRow() {
+                                            Image("sampleImage")
+                                                .resizable()
+                                                .frame(width: geometry.size.width/3, height: geometry.size.width/3)
+                                                .scaledToFit()
+                                            Image("sampleImage")
+                                                .resizable()
+                                                .frame(width: geometry.size.width/3, height: geometry.size.width/3)
+                                                .scaledToFit()
+                                        }
+                                        GridRow() {
+                                            Image("sampleImage")
+                                                .resizable()
+                                                .frame(width: geometry.size.width/3, height: geometry.size.width/3)
+                                                .scaledToFit()
+                                            Image("sampleImage")
+                                                .resizable()
+                                                .frame(width: geometry.size.width/3, height: geometry.size.width/3)
+                                                .scaledToFit()
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                    .padding(30)
                 }
+            }
         }
     }
 }
