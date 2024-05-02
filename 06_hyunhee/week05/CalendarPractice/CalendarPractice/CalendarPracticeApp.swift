186bc52647020b3be4f0cb349f7aab4dac9771c3
nonaninona <starcraft0529@gmail.com>
2024-05-02 17:21:05 +0900

    5주차 실습

---
+++ 06_hyunhee/week05/CalendarPractice/CalendarPractice/CalendarPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarPracticeApp.swift
+//  CalendarPractice
+//
+//  Created by lee hyunhee on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
