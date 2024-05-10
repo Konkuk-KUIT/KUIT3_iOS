5a9b8b8b2d18bcc77e4fbfa94271f5bcacec4c6a
nonaninona <starcraft0529@gmail.com>
2024-05-10 10:19:56 +0900

    Feat : 6주차 실습 완료

---
+++ 06_hyunhee/week06/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by lee hyunhee on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
