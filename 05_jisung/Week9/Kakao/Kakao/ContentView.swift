54b05d929846da47d1ed827601053c163a21c466
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-05-16 18:50:06 +0900

    [Week9-jisung] commit

---
+++ 05_jisung/Week9/Kakao/Kakao/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kakao
+//
+//  Created by 임지성 on 6/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 05_jisung/Week9/Kakao/Kakao/KakaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoApp.swift
+//  Kakao
+//
+//  Created by 임지성 on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct KakaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
