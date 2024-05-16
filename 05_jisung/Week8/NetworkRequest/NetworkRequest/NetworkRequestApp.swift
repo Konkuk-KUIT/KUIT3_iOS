1d1e0676cf96f4adb1beb8b57b9a7dbf7d508fb4
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-05-16 18:50:06 +0900

    [Week8-jisung] 미션 완료

---
+++ 05_jisung/Week8/NetworkRequest/NetworkRequest/NetworkRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkRequestApp.swift
+//  NetworkRequest
+//
+//  Created by 임지성 on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
