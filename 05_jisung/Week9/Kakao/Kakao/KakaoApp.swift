04124d07b043362a392f04b78a16ad598d611b4a
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-06-01 13:33:22 +0900

    [Week9-jisung] 미션 완료

--- 05_jisung/Week9/Kakao/Kakao/KakaoApp.swift
+++ 05_jisung/Week9/Kakao/Kakao/KakaoApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct KakaoApp: App {
+    init() {
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey: "457995aa1d17adb3fae21b08035af901")
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // onOpenURL()을 사용해 커스텀 URL 스킴 처리
+            ContentView().onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    AuthController.handleOpenUrl(url: url)
+                }
+            })
         }
     }
 }
