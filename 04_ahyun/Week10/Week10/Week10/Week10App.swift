afe4ce202792e1c01967e4539fd83ffb69144659
ahhyun1015 <121007805+ahhyun1015@users.noreply.github.com>
2024-06-10 14:34:16 +0900

    [Week10-ahyun] 10주차 미션

---
+++ 04_ahyun/Week10/Week10/Week10/Week10App.swift
@@ -0,0 +1,17 @@
+//
+//  Week10App.swift
+//  Week10
+//
+//  Created by 정아현 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Week10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
